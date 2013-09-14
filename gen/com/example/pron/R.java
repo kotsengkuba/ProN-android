71bea46e7578439a15acc0ea3659b85ea4220efc
minnie pangilinan <minnie_pangilinan@tahoo.com.ph>
2013-09-15 03:15:23 +0800

    location api

--- gen/com/example/pron/R.java
+++ gen/com/example/pron/R.java
@@ -24,18 +24,41 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f08000f;
+        public static final int chanceRainLabel=0x7f08000a;
+        public static final int forecastImage=0x7f080008;
+        public static final int humidityLabel=0x7f08000b;
+        public static final int linearLayout1=0x7f080002;
+        public static final int location=0x7f080003;
+        public static final int relativeLayout1=0x7f08000c;
+        public static final int searchView1=0x7f080000;
+        public static final int seekBar1=0x7f080007;
+        public static final int tempLabel=0x7f080009;
+        public static final int textView1=0x7f080001;
+        public static final int textView2=0x7f080004;
+        public static final int textView3=0x7f080005;
+        public static final int textView4=0x7f080006;
+        public static final int tomorrowLabel=0x7f08000d;
+        public static final int tomorrowTemp=0x7f08000e;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_choose_location=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int choose_location=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int chance_of_rain=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int humidity=0x7f050006;
+        public static final int location_name=0x7f050003;
+        public static final int temperature=0x7f050004;
+        public static final int title_activity_choose_location=0x7f050008;
+        public static final int tomorrow=0x7f050007;
     }
     public static final class style {
         /** 
--- src/com/example/pron/MainActivity.java
+++ src/com/example/pron/MainActivity.java
@@ -4,19 +4,76 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements LocationListener{
+	protected LocationManager locationManager;
+	TextView text;
+	private String provider;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		text = (TextView) findViewById(R.id.location);
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+		
+		// Define the criteria how to select the locatioin provider -> use
+	    // default
+	    Criteria criteria = new Criteria();
+	    provider = locationManager.getBestProvider(criteria, false);
+	    Location location = locationManager.getLastKnownLocation(provider);
+	    
+	    // Initialize the location fields
+	    if (location != null) {
+	      System.out.println("Provider " + provider + " has been selected.");
+	      onLocationChanged(location);
+	    } else {
+	      text.setText("Location not available");
+	    }
+		//text.setText(LocationManager.GPS_PROVIDER+LOCATION_SERVICE+location);
 	}
-
+	
+	/* Request updates at startup */
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onResume() {
+	  super.onResume();
+	  locationManager.requestLocationUpdates(provider, 400, 1, this);
+	}
+	
+	/* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() {
+      super.onPause();
+      locationManager.removeUpdates(this);
+    }
+	
+	@Override
+	public void onLocationChanged(Location location) {
+		text.setText(location.toString());
+	}
+	
+	@Override
+	public void onProviderDisabled(String provider){
+		Toast.makeText(this, "Disabled provider " + provider,Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public void onProviderEnabled(String provider){
+		Toast.makeText(this, "Enabled new provider " + provider,Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras){
+		
 	}
 
 }
