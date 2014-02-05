43d7b442b5aa54b779cf34e94e58a622099b0a70
minnie pangilinan <minnie_pangilinan@tahoo.com.ph>
2014-02-05 16:12:46 +0800

    reduce wheel lag

--- src/com/example/pron/Wheel.java
+++ src/com/example/pron/Wheel.java
@@ -25,12 +25,15 @@ public class Wheel extends View{
 	public VelocityTracker mVelocityTracker = null;
 	
 	float width,height,cx,cy,r,delta,lasty,ex,ey,rad;
+	float nex, ney, angle;
 	boolean snap = true, onWheelArea = false;
 	int [] icon_array = new int[]{R.drawable.clear, R.drawable.cloudy, R.drawable.cloudy, R.drawable.rainy, R.drawable.rainy, R.drawable.rainy, R.drawable.clear, R.drawable.cloudy};
 	Paint p = new Paint();
 	int [] pie_colors_array = new int [] {Color.rgb(50,55,180),Color.rgb(50,55,160),Color.rgb(50,55,140),Color.rgb(50,55,120),Color.rgb(255,241,151),Color.rgb(255,236,95),Color.rgb(255,221,0),Color.rgb(242,201,0)};
 	int [] circle_colors_array = new int [] {Color.BLUE,Color.BLUE,Color.BLUE,Color.BLUE,Color.rgb(255,147,30),Color.rgb(255,147,30),Color.rgb(255,147,30),Color.rgb(255,147,30)};
-
+	RectF rect;
+	Bitmap [] bitmap_array;
+	
 	public Wheel(Context context) {
 	    super(context);
 	}
@@ -46,22 +49,31 @@ public class Wheel extends View{
 		display.getSize(size);
 		width = size.x;
 		height = size.y;
-		
+		bitmap_array = new Bitmap [8];		
 		cx = 0;
 		cy = height/2;
 		r = width/2;
 		ex = cx+r;
 		ey = cy;
 		rad = 0;
+		
+		rect = new RectF(cx-r, cy-r, cx+r, cy+r);
+			
     } 
     
     public void setIcons(int[] arr){
     	icon_array = arr;
+    	
+    	for(int i=0; i<8; i++){
+			Bitmap bmp = BitmapFactory.decodeResource(getResources(), icon_array[i]);
+			bmp = Bitmap.createScaledBitmap(bmp, 200, 200, true);
+			bitmap_array[i] = bmp;
+		}
     }
 
     protected void onDraw(Canvas canvas) { 
 		Log.d("paint","delta: "+delta);
-		Log.d("paint","diameter: "+(2*r));
+		//Log.d("paint","diameter: "+(2*r));
 				
 		
 		ex = cx + (float) (r*Math.cos(rad));
@@ -73,10 +85,6 @@ public class Wheel extends View{
 		p.setAntiAlias(true);
 		p.setStyle(Paint.Style.FILL_AND_STROKE); 
 		p.setStrokeWidth(4.5f);
-		RectF rect = new RectF(cx-r, cy-r, cx+r, cy+r);
-		
-						
-		float nex, ney, angle;
 		
 		for(int i=0; i<8; i++){
 			angle = (float) (i*(Math.PI/4));
@@ -85,12 +93,12 @@ public class Wheel extends View{
 			//canvas.drawLine(cx, cy, nex, ney, p);
 
 			p.setColor(pie_colors_array[i]);
-			p.setStyle(Paint.Style.FILL_AND_STROKE);
+			p.setStyle(Paint.Style.FILL);
 			canvas.drawArc(rect, (((rad+angle)*180)/(float)Math.PI)%360, 45, true, p);
 			
-			p.setColor(Color.WHITE);
-			p.setStyle(Paint.Style.STROKE); 
-			canvas.drawArc(rect, (((rad+angle)*180)/(float)Math.PI)%360, 45, true, p);
+			//p.setColor(Color.WHITE);
+			//p.setStyle(Paint.Style.STROKE); 
+			//canvas.drawArc(rect, (((rad+angle)*180)/(float)Math.PI)%360, 45, true, p);
 		
 			blue = blue + 10;
 		}
@@ -101,11 +109,11 @@ public class Wheel extends View{
 			nex = cx + (float) ((r+10)*Math.cos(rad+angle));
 			ney = cy + (float) ((r+10)*Math.sin(rad+angle));
 			
-			Bitmap bmp = BitmapFactory.decodeResource(getResources(), icon_array[i]);
-			bmp = Bitmap.createScaledBitmap(bmp, 200, 200, true);
+			//Bitmap bmp = BitmapFactory.decodeResource(getResources(), icon_array[i]);
+			///bmp = Bitmap.createScaledBitmap(bmp, 200, 200, true);
 			
 			if(!(ney-1>=cy-r*Math.sin(Math.PI/4) && ney+1<=cy+r*Math.sin(Math.PI/4))){
-				bmp = Bitmap.createScaledBitmap(bmp, (int)(bmp.getWidth()*0.8), (int)(bmp.getHeight()*0.8), true);
+				Bitmap bmp = Bitmap.createScaledBitmap(bitmap_array[i], (int)(bitmap_array[i].getWidth()*0.8), (int)(bitmap_array[i].getHeight()*0.8), true);
 				p.setColor(circle_colors_array[i]);
 				p.setStyle(Paint.Style.FILL_AND_STROKE);
 				canvas.drawCircle(nex, ney, 90, p);
@@ -113,6 +121,9 @@ public class Wheel extends View{
 				p.setColor(Color.WHITE);
 				p.setStyle(Paint.Style.FILL_AND_STROKE);
 				canvas.drawCircle(nex, ney, 70, p);
+				
+				canvas.drawBitmap(bmp, nex-bmp.getWidth()/2, ney-bmp.getHeight()/2, null);
+
 			}
 			else{
 				p.setColor(circle_colors_array[i]);
@@ -122,8 +133,10 @@ public class Wheel extends View{
 				p.setColor(Color.WHITE);
 				p.setStyle(Paint.Style.FILL_AND_STROKE);
 				canvas.drawCircle(nex, ney, 100, p);
+				
+		        canvas.drawBitmap(bitmap_array[i], nex-bitmap_array[i].getWidth()/2, ney-bitmap_array[i].getHeight()/2, null);
+
 			}
-	        canvas.drawBitmap(bmp, nex-bmp.getWidth()/2, ney-bmp.getHeight()/2, null);
 		}
 		
     }
