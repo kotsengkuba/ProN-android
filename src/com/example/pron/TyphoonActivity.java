cb7107a08c63f7e126032e54dc0c19f5ef748c06
Gabrielle Ramirez <ecg_gabbi@yahoo.com>
2014-03-01 04:55:38 +0800

    Updated Typhoon Details page

--- src/com/example/pron/TyphoonActivity.java
+++ src/com/example/pron/TyphoonActivity.java
@@ -1,13 +1,21 @@
 package com.example.pron;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TyphoonActivity extends Activity{
 
+	TextView typhoonTextView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_typhoon);
+		
+		typhoonTextView = (TextView) findViewById(R.id.textView1);
+		Typeface font = Typeface.createFromAsset(getAssets(), "TRACK.OTF");
+		typhoonTextView.setTypeface(font);
 	}
 }
