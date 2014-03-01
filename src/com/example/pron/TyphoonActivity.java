fee8a55ef74777a723ae3bedbb25d4dd85f85bfa
Gabrielle Ramirez <ecg_gabbi@yahoo.com>
2014-03-01 10:04:30 +0800

    Added Map logo

--- src/com/example/pron/TyphoonActivity.java
+++ src/com/example/pron/TyphoonActivity.java
@@ -8,14 +8,24 @@ import android.widget.TextView;
 public class TyphoonActivity extends Activity{
 
 	TextView typhoonTextView;
+	//TyphoonActivityFragment fragment;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_typhoon);
 		
-		typhoonTextView = (TextView) findViewById(R.id.textView1);
+		typhoonTextView = (TextView) findViewById(R.id.header_typhoon);
 		Typeface font = Typeface.createFromAsset(getAssets(), "TRACK.OTF");
 		typhoonTextView.setTypeface(font);
+/*
+		fragment = (TyphoonActivityFragment) getFragmentManager().
+				findFragmentById(R.id.typhoon_detail_fragment);
+				if (fragment==null || ! fragment.isInLayout()) {
+				  // start new Activity
+				}
+				else {
+				  //fragment.update(...);
+				} */
 	}
 }
